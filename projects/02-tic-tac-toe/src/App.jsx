256f3fdacaeb2c109b4450678e44b2dc9831da28
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2024-07-23 13:08:46 -0500

    Update App.jsx

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,62 +1,64 @@
-import { useState } from "react";
-import { WinnerModal } from "./components/WinnerModal.jsx";
-import { GameBoard } from "./components/GameBoard.jsx";
-import { TURNS } from "./constants.js";
-import { checkWinner, checkEndGame, newTurn } from "./logic/board.js";
-import { Turn } from "./components/Turn.jsx";
+import { useState } from "react"
+import { WinnerModal } from "./components/WinnerModal.jsx"
+import { GameBoard } from "./components/GameBoard.jsx"
+import { TURNS } from "./constants.js"
+import { checkWinner, checkEndGame, newTurn } from "./logic/board.js"
+import { Turn } from "./components/Turn.jsx"
 
 function App() {
   //Definicion del tablero con 9 posiciones y rellenar en null
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
-    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
   })
 
   //Definicion de estado para los turnos, turno inicial y cambio de turno
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn');
-    return turnFromStorage ?? TURNS.X;
-  });
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   //Definicion de estado para establecer ganador
   //null no hay ganador, false empate
-  const [winner, setWinnner] = useState(null);
+  const [winner, setWinnner] = useState(null)
 
   //Reiniciar juego
   const resetGame = () => {
-    setBoard(Array(9).fill(null));
-    setTurn(TURNS.X);
-    setWinnner(null);
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinnner(null)
 
     window.localStorage.removeItem('board')
     window.localStorage.removeItem('turn')
-  };
+  }
 
   //Actualizacion del tablero
   const updateBoard = (index) => {
     //Evitar actualizacion de tablero si ya esta marcado
-    if (board[index]) return;
+    if (board[index]) return
 
     //Actualizar tablero despues de cada jugada
-    const newBoard = [...board];
-    newBoard[index] = turn;
-    setBoard(newBoard);w
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
     //Cambio de turno
-    setTurn(newTurn(turn));
+    setTurn(newTurn(turn))
 
     //Guardar partida
     window.localStorage.setItem('board', JSON.stringify(newBoard))
     window.localStorage.setItem('turn', newTurn(turn))
 
     //Revisar si hay ganador
-    const newWinner = checkWinner(newBoard);
+    const newWinner = checkWinner(newBoard)
     if (newWinner) {
-      setWinnner(newWinner);
+      setWinnner(newWinner)
     } else if (checkEndGame(newBoard)) {
-      setWinnner(false); //empate
+      setWinnner(false) //empate
     }
-  };
+  }
 
   return (
     <main className="board">
@@ -69,7 +71,7 @@ function App() {
 
       <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
